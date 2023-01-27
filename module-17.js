031cece7301d8a428ebb5c47a57305c4b2be401f
mahmudev <jonas67028@gmail.com>
2023-01-27 18:50:45 +0600

    day-2

---
+++ module-17.js
@@ -0,0 +1,33 @@
+// problem#1
+
+var money = 1000;
+var spend = 700;
+var returnMoney = money-spend;
+console.log(returnMoney)
+
+// problem#2
+
+var math  = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var average = (math + biology + chemistry + physics + bangla) / 5;
+console.log(average.toFixed(2))
+
+// problem#3
+
+var textOne = "I am going to be";
+var textwo = "an awesome web developer";
+var oneLine = textOne + " " + textwo;
+console.log(oneLine)
+
+// problem#4
+
+var inputNumber = 17
+var outpuNumber = inputNumber % 5;
+console.log(outpuNumber)
+
+
+
+
---
+++ module-18.js
@@ -0,0 +1,47 @@
+// problem#1
+
+var fruits = ["apple","banana","orange"];
+var result = fruits.indexOf("banana")
+var result = fruits.pop("banana");
+var result = fruits.push("watermelon");
+console.log(result)
+
+// problem#2
+
+let scoreMe = 85;
+let scoreTom = 66;
+let scoreJane = 95;
+let scorePeter = 56;
+let scoreJohn = 40;
+
+if (scoreMe >= 80) {
+    console.log("Your grade is A");
+} else if (scoreMe >= 60) {
+    console.log("Your grade is B");
+} else if (scoreMe >= 50) {
+    console.log("Your grade is C");
+} else if (scoreMe >= 40) {
+    console.log("Your grade is D");
+} else {
+    console.log("Your grade is F");
+}
+
+// problem#3
+
+if (13 > 79 && 13 > 45) {
+    console.log('13 is the largest number');
+  } 
+  else if (79 > 45) {
+    console.log('79 is the largest number');
+  } 
+  else {
+    console.log('45 is the largest number');
+  }
+
+  
+  if (9 === 8 || 8 === 9 || 9 === 9) {
+    console.log('This triangle is Isosceles!')
+  } else {
+    console.log('This triangle is not Isosceles')
+  }
+  
