d52ea462d9515423ce5b3637bd32799bff9eb4e6
mahmudev <jonas67028@gmail.com>
2023-01-30 20:02:50 +0600

    day-4

--- module-20.js
+++ module-20.js
@@ -1,16 +1,51 @@
-// problem#1
+// ১. একটা ফাংশন লিখবা যেটা ১৩ এর নামতা (multiplication table) আউটপুট হিসেবে দেখাবে। 
 
-function make_avg(numbers) {
-  var total = 0;
-  for (var i = 0; i < numbers.length; i++) {
-    var number = numbers[i];
-    total = total + number;
+function displayMultiplicationTable(number) {
+  for (var i = 1; i <= 10; i++) {
+    const result = i * number;
+    console.log(result)
   }
-  var average = total / numbers.length;
-  return average;
 }
-var arrayNum = [5, 7, 8, 5, 6, 4, 8, 0];
-console.log(make_avg(arrayNum));
+displayMultiplicationTable(13)
+
+// ২. একটা ফাংশন লিখবা যেটা যেকোন নামকে uppercase বা রেগুলার কেইস হিসেবে নিবে আর আউটপুট হিসেবে সেই নাম lowercase করে রিটার্ন করবে।
+
+function makeLowerCase(text){
+  let result = text.toLowerCase();
+  console.log(result)
+}
+makeLowerCase("THIS IS TEST")
+
+// ৩. fullName নামে একটা ফাংশন তৈরী কর যেটা তোমার নামের প্রথম অংশ এবং তোমার নামের শেষের অংশ প্যারামিটার হিসেবে নিবে। আর তোমার নামের দুই অংশ জোড়া দিয়ে আউটপুট হিসেবে তোমার পূর্ন নাম রিটার্ন করে দিবে।
+
+function fullName(firstName, lastName){
+  var result = firstName + " " +lastName;
+  console.log(result)
+}
+fullName("Mahmud", "Islam")
 
 
+// ৪. একটা ফাংশন লিখবা যেটাকে তুমি কোন সংখ্যাকে ইনপুট হিসেবে দিলে সে সেই সংখ্যার square করে সেই square কে রিটার্ন করবে।
 
+function makeSquare(number){
+  var result = number*number;
+  console.log(result)
+}
+makeSquare(5)
+
+// ৫) pizza নামের একটা object কে নিচের উদাহরণ মতো define করবা :
+
+// const pizza = {
+//  toppings: ['cheese', 'sauce', 'pepperoni'],
+//  crust: 'deep dish',
+//  serves: 2
+// }
+// এবং pepperoni প্রিন্ট করবা।
+
+const pizza = {
+ toppings: ['cheese', 'sauce', 'pepperoni'],
+ crust: 'deep dish',
+ serves: 2
+}
+var result = pizza.toppings[2];
+console.log(result);
