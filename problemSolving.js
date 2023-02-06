1a674cc3debc6e9bb7fa434aef916d7b8c3b9024
mahmudev <jonas67028@gmail.com>
2023-02-06 13:25:45 +0600

    day-8

---
+++ problemSolving.js
@@ -0,0 +1,40 @@
+// 1 থেকে n পর্যন্ত সংখ্যার যোগফল নির্ণয় করো।
+
+// function totalSum(number) {
+//   let sum = 0;
+//   for (let i = 1; i <= number; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// }
+// alternative way using (n*(n+1))/2
+
+// function sumOfn(n) {
+//   let result = (n * (n + 1)) / 2;
+//   return result;
+// }
+
+// console.log(sumOfn(10))
+
+// console.log(totalSum(10));
+
+// ৩ এর নামতা লিখো জাভাস্ক্রিপ্ট ব্যাবহার করে।
+
+// function timesTable(n) {
+//   for (let i = 1; i <= 10; i++) {
+//     console.log(i * n);
+//   }
+// }
+// timesTable(3)
+
+
+// এমন একটা ফাংশন বানাও যেটা একটি অ্যারে নিবে ইনপুট হিসেবে। তারপর সেই অ্যারের সকল উপাদান এর মধ্যে ৫ এর চেয়ে বড় যে সকল সংখ্যা আছে তাদের কাউন্ট নির্ণয় করবে।
+
+// ইনপুটঃ [-1,2,-3,4,5,6,-7,8,-9,10]
+
+
+// আউটপুটঃ 3
+
+let number = [-1,2,-3,4,5,6,-7,8,-9,10]
+
+
